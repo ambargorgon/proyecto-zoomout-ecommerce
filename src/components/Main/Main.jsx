1aab2f04b5a017b74c1e80f0a8190c91168f28c7
unknown <ambargorgon@gmail.com>
2022-09-29 11:26:59 -0300

    arreglos

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,8 @@ import ItemListContainer from "./ItemListContainer/ItemListContainer";
 import "./main.css";
 import { Routes, Route } from "react-router-dom";
 import Form from "./Form/Form";
-import Cart from './Cart/Cart';
+import Cart from "./Cart/Cart";
+
 
 const Main = () => {
   return (
@@ -13,8 +14,8 @@ const Main = () => {
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/item/:id" element={<ItemDetailContainer />} />
         <Route path="/category/:id" element={<ItemListContainer />} />
-        <Route path="/cart" element={<Cart />} />
         <Route path='/form' element={<Form />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </main>
   );
